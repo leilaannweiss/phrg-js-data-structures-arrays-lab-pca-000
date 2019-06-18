90d172e6c98e1efa539e024a643a246ba94c4547
Leila Weiss <leila.weiss@powerhrg.com>
2019-06-18 08:24:48 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendDriver(name) {
+  drivers.push("Ralph");
+}
+
+function destructivelyPrependDriver(name) {
+  drivers.unshift("Bob");
+}
+
+function destructivelyRemoveLastDriver() {
+  drivers.pop();
+}
+
+function destructivelyRemoveFirstDriver() {
+  drivers.shift();
+}
+
+function appendDriver(name) {
+  const newDrivers = [...drivers,name];
+  return newDrivers
+}
+
+function prependDriver(name) {
+  const newDrivers = [name, ...drivers];
+  return newDrivers
+}
+
+function removeLastDriver() {
+  const lastDriver = drivers.slice(0, 2);
+  return lastDriver
+}
+
+function removeFirstDriver() {
+  const firstDriver = drivers.slice(1);
+  return firstDriver
+}
